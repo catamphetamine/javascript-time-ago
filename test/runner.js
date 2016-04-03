dcf40f87d1b7ff78b42d593e6362ff3fd4603376
halt-hammerzeit <kuchumovn@gmail.com>
2016-04-03 23:26:33 +0300

    misc intl.js startup code

--- test/runner.js
+++ test/runner.js
@@ -1,5 +1,20 @@
-if (typeof global.Intl === 'undefined')
+import are_intl_locales_supported from 'intl-locales-supported'
+
+if (global.Intl)
 {
+	// Determine if the built-in `Intl` has the locale data we need.
+	if (!are_intl_locales_supported(['en', 'ru']))
+	{
+		// `Intl` exists, but it doesn't have the data we need, so load the
+		// polyfill and patch the constructors we need with the polyfill's.
+		const Intl_polyfill = require('intl')
+		Intl.NumberFormat   = Intl_polyfill.NumberFormat
+		Intl.DateTimeFormat = Intl_polyfill.DateTimeFormat
+	}
+}
+else
+{
+	// No `Intl`, so use and load the polyfill.
 	global.Intl = require('intl')
 }
 
