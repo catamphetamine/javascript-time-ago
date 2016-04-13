c5e030680671c6c400e48ecb3c9d610b1a3f344d
Nikolay <halt.hammerzeit.at@gmail.com>
2016-04-13 14:55:14 +0300

    fix

--- locales.js
+++ locales.js
@@ -1,6 +1,7 @@
+var path = require('path')
 var fs = require('fs')
 
-fs.readdirSync('./locales').forEach(function(file)
+fs.readdirSync(path.resolve(__dirname, 'locales')).forEach(function(file)
 {
 	var locale = file.replace(/\.js$/, '')
 	javascript_time_ago.locale(locale, require('./locales/' + locale))
