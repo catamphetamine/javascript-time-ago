b692fdd95f74347799aadff84408705203b4861f
Nikolay <halt.hammerzeit.at@gmail.com>
2016-04-16 20:15:12 +0300

    Addressed #1

--- index.es6.js
+++ index.es6.js
@@ -1,12 +1,11 @@
 import javascript_time_ago from './source/time ago'
 
+export { a_day, days_in_a_month, days_in_a_year, gradation } from './source/classify elapsed'
+export default javascript_time_ago
+
 // Add all locale data to `javascript-time-ago`.
 // This module will be ignored when bundling 
 // for the browser with Browserify/Webpack.
 global.javascript_time_ago = javascript_time_ago
 require('./locales')
 delete global.javascript_time_ago
-
-// export {  default, from_CLDR } from './source/time ago'
-export default javascript_time_ago
-export { a_day, days_in_a_month, days_in_a_year, gradation } from './source/classify elapsed'
