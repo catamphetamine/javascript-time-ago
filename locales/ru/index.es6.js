66dccb9831f32150bcc3e4e85f125be31e968f66
catamphetamine <purecatamphetamine@gmail.com>
2018-01-08 01:40:33 +0300

    hyphenating locale json names

--- locales/ru/index.es6.js
+++ locales/ru/index.es6.js
@@ -1,7 +1,7 @@
 export { default as long } from './long.json'
-export { default as long_concise } from './long concise.json'
+export { default as long_concise } from './long-concise.json'
 export { default as short } from './short.json'
-export { default as short_concise } from './short concise.json'
+export { default as short_concise } from './short-concise.json'
 export { default as tiny } from './tiny.json'
 
 // `const` is not supported in Internet Explorer 10
--- locales/ru/index.js
+++ locales/ru/index.js
@@ -1,7 +1,7 @@
 var long          = require('./long.json')
-var long_concise  = require('./long concise.json')
+var long_concise  = require('./long-concise.json')
 var short         = require('./short.json')
-var short_concise = require('./short concise.json')
+var short_concise = require('./short-concise.json')
 var tiny          = require('./tiny.json')
 
 module.exports =
