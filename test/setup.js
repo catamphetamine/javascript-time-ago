a4c817a8f2f41eb41b8874fb892d3c992261eedf
catamphetamine <purecatamphetamine@gmail.com>
2018-01-08 23:42:04 +0300

    Fixing tests

--- test/setup.js
+++ test/setup.js
@@ -11,19 +11,19 @@ const locales = ['en', 'ru']
 
 if (global.Intl)
 {
-	// // Has a bug: "SyntaxError: Invalid regular expression: ...".
-	// // https://github.com/andyearnshaw/Intl.js/issues/256#issuecomment-349335699
-	// // Determine if the built-in `Intl` has the locale data we need
-	// if (!are_intl_locales_supported(locales))
-	// {
-	// 	// `Intl` exists, but it doesn't have the data we need, so load the
-	// 	// polyfill and patch the constructors we need with the polyfill's
-	// 	global.Intl.NumberFormat   = Intl_polyfill.NumberFormat
-	// 	global.Intl.DateTimeFormat = Intl_polyfill.DateTimeFormat
-	// }
+	// Has a bug: "SyntaxError: Invalid regular expression: ...".
+	// https://github.com/andyearnshaw/Intl.js/issues/256#issuecomment-349335699
+	// Determine if the built-in `Intl` has the locale data we need
+	if (!are_intl_locales_supported(locales))
+	{
+		// `Intl` exists, but it doesn't have the data we need, so load the
+		// polyfill and patch the constructors we need with the polyfill's
+		global.Intl.NumberFormat   = Intl_polyfill.NumberFormat
+		global.Intl.DateTimeFormat = Intl_polyfill.DateTimeFormat
+	}
 
-	// Using this workaround for now
-	global.Intl = Intl
+	// `Intl` property is read-only
+	// global.Intl = Intl
 }
 else
 {
