89c330c13c4b5cd79612a3dab8cec19d8bf6a10b
catamphetamine <purecatamphetamine@gmail.com>
2018-01-09 00:55:38 +0300

    misc refactoring

--- load-all-locales.js
+++ load-all-locales.js
@@ -7,8 +7,7 @@ var javascript_time_ago = require('javascript-time-ago').default
 fs.readdirSync(path.resolve(__dirname, 'locale')).forEach(function(filename)
 {
 	var locale_path = path.resolve(__dirname, 'locale', filename)
-	var stats = fs.statSync(locale_path)
-	if (stats.isDirectory())
+	if (fs.statSync(locale_path).isDirectory())
 	{
 		javascript_time_ago.locale(require(locale_path))
 	}
