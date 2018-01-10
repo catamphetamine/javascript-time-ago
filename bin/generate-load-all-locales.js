c027ebc42b070feb40fcee90fa59bcb61e838aa5
catamphetamine <purecatamphetamine@gmail.com>
2018-01-10 03:43:31 +0300

    Refactored "load-all-locales"

--- bin/generate-load-all-locales.js
+++ bin/generate-load-all-locales.js
@@ -17,9 +17,15 @@ fs.readdirSync(path.resolve(__dirname, '../locale')).forEach(function(filename)
 fs.writeFileSync(path.resolve(__dirname, '../load-all-locales.js'),
 `
 var TimeAgo = require("javascript-time-ago")
-
-${locales.map(locale => 'var ' + locale + ' = require("javascript-time-ago/locale/' + locale + '")').join('\n')}
-
-${locales.map(locale => 'TimeAgo.locale(' + locale + ')').join('\n')}
+${locales.map(locale => 'TimeAgo.locale(require("javascript-time-ago/locale/' + locale + '"))').join('\n')}
 `
 .trim())
+
+// ES6
+// `
+// import TimeAgo from "javascript-time-ago"
+//
+// ${locales.map(locale => 'import ' + locale + ' from "javascript-time-ago/locale/' + locale + '"').join('\n')}
+//
+// ${locales.map(locale => 'TimeAgo.locale(' + locale + ')').join('\n')}
+// `
